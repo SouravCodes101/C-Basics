924b310e317cf1dae17a6e81a592a75ca4f6e2d2
Sourav Choudhury <62167988+SouravCodes101@users.noreply.github.com>
2021-06-02 11:46:11 +0530

    initial commit

---
+++ chapter 1/first.c
@@ -0,0 +1,14 @@
+# include<stdio.h> 
+
+/*
+This is our first C Program
+which is awesome!
+*/
+int main(){
+    int tom;
+    int Tom;
+    int error_code;
+    // 'J' --> A Character
+    printf("Hello , Iam learning C!");
+    return 0;
+}
